4d7bb1320ccf26f1715146f448301f11fca78224
sydwang <wangyijun19@gmail.com>
2016-11-16 15:28:04 +1100

    add first file
    this is the original version

---
+++ 12.3.1_02.php
@@ -0,0 +1,15 @@
+<?php
+    $file_name = "test.txt";
+    $fp = fopen($file_name,  'r');
+    if ($fp) {
+      $data = fread($fp,  filesize($file_name));
+      fclose($fp);
+      $file_name = "ftp://user:pass@ftp.myserver.com/usr/home/dj/$file_name"; 
+      $fp = fopen($file_name,  'wt');
+      if ($fp) {
+        echo 'ÕýÔÚÐ´Èë...';
+        fwrite($fp,  $data);
+        fclose($fp);
+      }
+    }
+?>
